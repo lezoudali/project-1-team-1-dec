d66b8099ca1c8100d8bf66b4244c8edbc42cf2ac
iair <i.linker8@gmail.com>
2024-01-24 19:08:48 -0300

    fix on the current weather process

--- etl_project/assets/sql/transform_current_weather/current_weather.sql
+++ etl_project/assets/sql/transform_current_weather/current_weather.sql
@@ -5,10 +5,9 @@
 
 select
     cast('{{ incremental_value }}' as date) as date,
-    sum(cast(HasPrecipitation as int)) as count_precipitations_last_five_days,
-    avg('uv_index') as avg_uv_index
+    sum(cast(has_precipitation as int)) as count_precipitations_last_five_days
 from 
     {{ source_table_name }}
 {% if is_incremental %}
-    where {{ config["incremental_column"] }} >= DATE_SUB('{{ incremental_value }}',INTERVAL 5 DAY)
+    where {{ config["incremental_column"] }} >= current_date - INTERVAL '5 DAYS'
 {% endif %}
