7ad6e0c14868c0ba44a3ca51c19b058a0d0f3bd0
iair <i.linker8@gmail.com>
2024-01-24 01:24:20 -0300

    add current weather to the pipeline (not tested)

--- etl_project/pipelines/accuweather.py
+++ etl_project/pipelines/accuweather.py
@@ -17,13 +17,15 @@ from etl_project.connectors.accuweather import AccuWeatherApiClient
 from etl_project.connectors.postgresql import PostgreSqlClient
 from etl_project.assets.accuweather import (
     extract_forecast_weather,
+    extract_current_weather,
     raw_data_transform,
+    raw_current_conditions_transform,
     staging_upsert_load,
+    uv_index_category
 )
 from etl_project.assets.transform_load import transform_load
 from etl_project.assets.pipeline_logging import PipelineLogging
 from etl_project.assets.metadata_logging import MetaDataLogging, MetaDataLoggingStatus
-
 
 if __name__ == "__main__":
     # load environment variables
@@ -65,13 +67,21 @@ if __name__ == "__main__":
             location_key=config.get("location_key"),
             forecast_days=config.get("forecast_days"),
         )
+        df_current_conditions = extract_current_weather(
+            accuweather_client = accuweather_client, 
+            location_name = config.get("location_name")
+            )
 
         # transform raw data
         pipeline_logging.logger.info("Transforming raw data into dataframe")
         df_clean_forecast = raw_data_transform(
             df_forecast=df_forecast,
-            location_key=config.get("location_key"),
-        )
+            location_key=config.get("location_key")
+            )
+        df_clean_current_conditions = raw_current_conditions_transform(
+            df_current_conditions = df_current_conditions, 
+            uv_index_category_func = uv_index_category
+            )
 
         # load data to staging
         pipeline_logging.logger.info("Loading data into staging DB table")
@@ -152,9 +162,62 @@ if __name__ == "__main__":
             target_table_name=serving_table,
             metadata=serving_metadata,
         )
+        # metadata_logger.log(
+        #     status=MetaDataLoggingStatus.RUN_SUCCESS, logs=pipeline_logging.get_logs()
+        # )
+        
+        staging_metadata_current_conditions = MetaData()
+        staging_table_current_conditions = Table(
+            config.get("staging_table_name_current_conditions"),
+            staging_metadata_current_conditions,
+            Column("date", Date, primary_key=True),
+            Column("location_key", Integer, primary_key=True),
+            Column("location_name", String, primary_key=True),
+            Column('weather_text', String),
+            Column('has_precipitation', Float),
+            Column('precipitation_type', String),
+            Column('precipitation_value', Float),
+            Column('temperature', Float),
+            Column('real_feel_temperature', Float),
+            Column('relative_humidity', Float),
+            Column('wind_dewpoint_direction', String),
+            Column('wind_speed', Float),
+            Column('uv_index', Integer),
+            Column('visibility', Float),
+            Column('pressure', Float),
+            Column('precipition_value', Float),
+            Column('uv_index_category', String)
+        )
+        staging_upsert_load(
+            dataframe=df_clean_current_conditions,
+            postgresql_client=postgresql_client,
+            table=staging_table_current_conditions,
+            metadata=staging_metadata_current_conditions,
+        )
+        
+        # create serving table based off staging table
+        pipeline_logging.logger.info("Creating serving DB table for current conditions")
+        serving_metadata_current_conditions = MetaData()
+        serving_table_current_conditions = Table(
+            config.get("serving_table_name_current_conditions"),
+            serving_metadata_current_conditions,
+            Column("date", Date, primary_key=True),
+            Column("location_key", Integer, primary_key=True),
+            Column("location_name", String, primary_key=True),
+            Column('uv_index_category', String),
+        )
+        
+        transform_load(
+            environment_path=config.get("transform_template_path_current_conditions"),
+            postgresql_client=postgresql_client,
+            source_table_name=config.get("staging_table_name_current_conditions"),
+            target_table_name=serving_table_current_conditions,
+            metadata=serving_metadata_current_conditions,
+        )
         metadata_logger.log(
             status=MetaDataLoggingStatus.RUN_SUCCESS, logs=pipeline_logging.get_logs()
         )
+        
     except BaseException as e:
         pipeline_logging.logger.error(f"Pipeline run failed. See detailed logs: {e}")
         metadata_logger.log(
